1fb98266992b224af02e62370ad67e5fa8563693
muruoawa <97142912+muruoawa@users.noreply.github.com>
2024-07-11 11:43:17 +0800

    Create 7111.js

---
+++ 7111.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         达加马专用
+// @namespace    https://b23.tv/22UnV4z
+// @version      7.11.1
+// @description  登录后点击上方按钮然后点击语文再点击其他科即可
+// @author       慕若
+// @run-at       document-start
+// @match        http*://test.k12media.cn:8000/tqweb/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    const button = document.createElement('button');
+button.className = 'text_lb';
+button.textContent = '启动！';
+button.style.fontSize = '16px'; // 调整字体大小
+button.style.color = 'red'; // 设置字体颜色
+button.onclick = function() {
+    viewTest('/tqweb/report/NewStudentReport.a?doShowStudentReport',91010);
+}
+// 将按钮添加到页面最上面
+document.body.prepend(button);
+})();
